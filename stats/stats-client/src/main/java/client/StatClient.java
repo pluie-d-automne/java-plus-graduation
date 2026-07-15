5129e0b35b99f77ca18faea934b81a705c68dbbb
Test <test@example.com>
2026-07-15 19:22:09 +0300

    Add Spring Cloud services

--- stats/stats-client/src/main/java/client/StatClient.java
+++ stats/stats-client/src/main/java/client/StatClient.java
@@ -3,67 +3,19 @@ package client;
 import ewm.HitDto;
 import ewm.ParamDto;
 import ewm.StatsDto;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
-@Slf4j
-@Component
-public class StatClient {
-    final RestClient restClient;
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+@FeignClient(name = "stats-server")
+public interface StatClient {
+    @PostMapping("/hit")
+    public void createHit(@Valid @RequestBody HitDto hitDto);
 
-    public StatClient(@Value("${stats.server.url}") String baseUrl) {
-        this.restClient = RestClient.builder()
-                .baseUrl(baseUrl)
-                .build();
-    }
-
-    public void hit(HitDto hitDto) {
-        try {
-            ResponseEntity<Void> response = restClient.post()
-                    .uri("/hit")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(hitDto)
-                    .retrieve()
-                    .toBodilessEntity();
-        } catch (Exception e) {
-            log.info("Failed to connect to stat service");
-        }
-    }
-
-    public List<StatsDto> get(ParamDto paramDto) {
-        List<StatsDto> stats;
-
-        Optional<List<String>> uris = (paramDto.uris() == null || paramDto.uris().isEmpty())
-                ? Optional.empty()
-                : Optional.of(paramDto.uris());
-
-        try {
-            stats = restClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/stats")
-                            .queryParam("start", paramDto.start().format(formatter))
-                            .queryParam("end", paramDto.end().format(formatter))
-                            .queryParamIfPresent("uris", uris)
-                            .queryParamIfPresent("unique", Optional.ofNullable(paramDto.unique()))
-                            .build())
-                    .header("Content-Type", "application/json")
-                    .retrieve()
-                    .body(new ParameterizedTypeReference<>() {
-                    });
-        } catch (Exception e) {
-            log.info("Failed to connect to stat service");
-            stats = List.of(new StatsDto("ewm-main-service", "/fake-uri", 0L));
-        }
-        return stats;
-    }
+    @GetMapping("/stats")
+    public List<StatsDto> getStats(@Valid @RequestBody ParamDto paramDto);
 }
--- stats/stats-server/src/main/java/ewm/controller/StatsController.java
+++ stats/stats-server/src/main/java/ewm/controller/StatsController.java
@@ -1,14 +1,16 @@
 package ewm.controller;
 
+import client.StatClient;
 import ewm.HitDto;
 import ewm.ParamDto;
 import ewm.StatsDto;
 import ewm.service.StatsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,19 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-public class StatsController {
+@FeignClient(name = "stats-server")
+public class StatsController implements StatClient {
     private final StatsService statsService;
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
     public void createHit(@Valid @RequestBody HitDto hitDto) {
+        log.info("Запрошен hit: {}", hitDto);
         statsService.createHit(hitDto);
     }
 
     @GetMapping("/stats")
-    public List<StatsDto> getStats(@Valid @ModelAttribute ParamDto paramDto) {
+    public List<StatsDto> getStats(@Valid @RequestBody ParamDto paramDto) {
+        log.info("Запрошено получение статистики: {}", paramDto);
         return statsService.getStats(paramDto);
     }
 }
