fbe840a2b1e3294fd73d085653cb10fdd41d2daa
Test <test@example.com>
2026-07-21 16:31:39 +0300

    Add custom feign decoder

--- core/event-service/src/main/java/ewm/core/EventApp.java
+++ core/event-service/src/main/java/ewm/core/EventApp.java
@@ -1,9 +1,12 @@
 package ewm.core;
 
+import ewm.core.client.FeignErrorDecoder;
+import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -11,5 +14,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class EventApp {
     public static void main(String[] args) {
         SpringApplication.run(EventApp.class, args);
+    }
+
+    @Bean
+    public Feign.Builder feignBuilder() {
+        return Feign.builder().errorDecoder(new FeignErrorDecoder());
     }
 }
